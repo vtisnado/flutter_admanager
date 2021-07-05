ff20d4d55e5342587e8de822a6aa6be78fdf1597
Victor T <vtisnado@Macbook.local>
2021-07-05 13:00:18 -0500

    Main errors fixed

--- ios/Classes/InterstitialAd.swift
+++ ios/Classes/InterstitialAd.swift
@@ -1,7 +1,8 @@
 import GoogleMobileAds
 
-class InterstitialAd: SwiftFlutterGoogleAdManagerPlugin {
-    private var interstitialAd: GAMInterstitialAd?
+class InterstitialAd: SwiftFlutterAdManagerPlugin {
+    // private var interstitialAd: GAMInterstitialAd?
+    var interstitial: GAMInterstitialAd
     private let channel: FlutterMethodChannel!
 
     public init(with _: FlutterPluginRegistrar, channel: FlutterMethodChannel) {
@@ -29,19 +30,38 @@ class InterstitialAd: SwiftFlutterGoogleAdManagerPlugin {
     private func load(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let argument = call.arguments as! Dictionary<String, Any>
         let isDevelop = argument["isDevelop"] as? Bool ?? false
-        let customTargeting = argument["customTargeting"] as? [String: Any]
+        let customTargeting = argument["customTargeting"] as? [String: String]
 
+        let adUnitId = argument["adUnitId"] as! String
         if isDevelop {
-            interstitialAd = GAMInterstitialAd(adUnitID: "/6499/example/interstitial")
+            // interstitialAd = GAMInterstitialAd(adUnitID: "/6499/example/interstitial")
+            // interstitialAd.load(withAdUnitID: "/6499/example/interstitial")
+            // let adId = "/6499/example/interstitial"
+            interstitial.adUnitID = "/6499/example/interstitial"
         } else {
-            let adUnitId = argument["adUnitId"] as! String
-            interstitialAd = GAMInterstitialAd(adUnitID: adUnitId)
+            
+            // interstitialAd = GAMInterstitialAd(adUnitID: adUnitId)
+            // interstitialAd.load(withAdUnitID: adUnitId)
+            // let adId = adUnitId
+            interstitial.adUnitID = adUnitId
         }
+        
         interstitialAd!.delegate = self
 
         let request = GAMRequest()
         request.customTargeting = customTargeting
         interstitialAd!.load(request)
+        // interstitialAd!.load(
+        //     withAdUnitID: adUnitId, 
+        //     request: request,
+        //     completionHandler: { [self] ad, error in
+        //         if let error = error {
+        //             print("Failed to load interstitial ad with error: \(error.localizedDescription)")
+        //             return
+        //         }
+        //         interstitial = ad
+        //     })
+
         result(nil)
     }
 
@@ -65,36 +85,36 @@ class InterstitialAd: SwiftFlutterGoogleAdManagerPlugin {
     }
 }
 
-extension InterstitialAd: GADInterstitialDelegate {
+extension InterstitialAd: GADFullScreenContentDelegate {
     /// Tells the delegate an ad request succeeded.
-    func interstitialDidReceiveAd(_ ad: GADInterstitial) {
+    func interstitialDidReceiveAd(_ ad: GAMInterstitialAd) {
         channel.invokeMethod("onAdLoaded", arguments: nil)
     }
 
     /// Tells the delegate an ad request failed.
-    func interstitial(_: GADInterstitial, didFailToReceiveAdWithError error: GADRequestError) {
+    func interstitial(_: GAMInterstitialAd, didFailToReceiveAdWithError error: NSError) {
         channel.invokeMethod("onAdFailedToLoad", arguments: ["errorCode": error.code])
     }
 
     /// Tells the delegate that an interstitial will be presented.
-    func interstitialWillPresentScreen(_: GADInterstitial) {
+    func interstitialWillPresentScreen(_: GAMInterstitialAd) {
         channel.invokeMethod("onAdOpened", arguments: nil)
     }
 
     /// Tells the delegate the interstitial is to be animated off the screen.
-    func interstitialWillDismissScreen(_ ad: GADInterstitial) {
-        interstitialAd = GAMInterstitialAd(adUnitID: ad.adUnitID!)
+    func interstitialWillDismissScreen(_ ad: GAMInterstitialAd) {
+        interstitialAd = GAMInterstitialAd(withAdUnitID: ad.adUnitID!)
         interstitialAd?.delegate = self
     }
 
     /// Tells the delegate the interstitial had been animated off the screen.
-    func interstitialDidDismissScreen(_: GADInterstitial) {
+    func interstitialDidDismissScreen(_: GAMInterstitialAd) {
         channel.invokeMethod("onAdClosed", arguments: nil)
     }
 
     /// Tells the delegate that a user click will open another app
     /// (such as the App Store), backgrounding the current app.
-    func interstitialWillLeaveApplication(_: GADInterstitial) {
+    func interstitialWillLeaveApplication(_: GAMInterstitialAd) {
         channel.invokeMethod("onAdLeftApplication", arguments: nil)
     }
 }
--- ios/Classes/SwiftFlutterAdManagerPlugin.swift
+++ ios/Classes/SwiftFlutterAdManagerPlugin.swift
@@ -4,5 +4,8 @@ import UIKit
 public class SwiftFlutterAdManagerPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
         registrar.register(BannerViewFactory(messenger: registrar.messenger()), withId: "plugins.adrianlogue.com/admanager/banner")
+        
+        let interstitialChannel = FlutterMethodChannel(name: "plugins.adrianlogue.com/admanager/interstitial", binaryMessenger: registrar.messenger())
+        registrar.addMethodCallDelegate(InterstitialAd(with: registrar, channel: interstitialChannel) as FlutterPlugin, channel: interstitialChannel)
     }
 }
